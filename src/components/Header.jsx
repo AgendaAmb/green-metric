093b6902f109f63ee4002b937270b5b790d5beac
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-06-16 11:08:43 -0600

    Fixing layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import Navbar from "./Navbar";
 
 function Header() {
     return (
-        <>
-            <div className="header">
+        <div className="header">
+            <div className="titles">
                 <h3>Agenda Ambiental</h3>
                 <h1>Green Metric</h1>
                 <h3>Universidad Autónoma de San Luis Potosí</h3>
             </div>
             <Navbar />
-        </>
+        </div>
     );
 }
 
