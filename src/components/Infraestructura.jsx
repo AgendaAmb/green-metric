551c73df99009a3408d8fcb72d456b53eafa21a9
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-06-21 14:28:33 -0600

    Finishing first module on Infraestructura

--- src/components/Infraestructura.jsx
+++ src/components/Infraestructura.jsx
@@ -21,11 +21,23 @@ export default function Infraestructura() {
     const [species, setSpecies] = useState(1);
     const data = 
         [
-        {text: "Área total de su entidad o espacio de trabajo.", type: 5, evidence: true},	
-        {text: "Área total de áreas verdes en su entidad o espacio de trabajo", type: 5, evidence: true},								
-        {text: "Área total de cobertura plantada", type: 5, evidence: true},								
-        {text: "Área total de cobertura forestal", type: 5, evidence: true},								
-        {text: "Área total de su entidad donde se lleven a cabo actividades académicas (incluyendo edificios administrativos, edificios para actividades estudiantiles y del personal, salones, jardines únicamente con fines académicos).", type: 5, evidence: true}
+        {text: "Área total de su entidad o espacio de trabajo.", type: 5},	
+        {text: "Área total de áreas verdes en su entidad o espacio de trabajo", type: 5, },								
+        {text: "Área total de cobertura plantada", type: 5, },								
+        {text: "Área total de cobertura forestal", type: 5, },								
+        {text: "Área total de su entidad donde se lleven a cabo actividades académicas (incluyendo edificios administrativos, edificios para actividades estudiantiles y del personal, salones, jardines únicamente con fines académicos).", type: 5, evidence: true, span: true},
+        {text: "¿Cuántos edificios componen tu entidad?", type: 5, },
+        {text: "Área total de la planta baja de los edificios de su entidad de trabajo.", type: 5, },
+        {text: "Área total construida de su entidad de trabajo (tome en cuenta los distintos niveles que conforman las edificaciones).", type: 5, },
+        {text: "Área correspondiente a jardines verticales.", type: 5, evidence: true, span: true},
+        {text: "Área de techos verdes.", type: 5, evidence: true},
+        {text: "Área de azoteas útiles (con accesos seguros, multiusos, etc).", type: 5, evidence: true},
+        {text: "Área destinada a la siembra de alimentos y plantas medicinales", type: 5, evidence: true},
+        {text: "Área destinada a la siembra de plantas que promueven la polinización.", type: 5, evidence: true},
+        {text: "Área de estacionamientos sin asfalto o concreto; pueden tener grava, adoquín o asfalto permeable.", type: 5, evidence: true},
+        {text: "Área total en el campus destinada para la absorción de agua; además de bosque y vegetación plantada. (suelo, césped, bloque de hormigón, campo sintético, etc.)", type: 5, evidence: true},
+        {text: "Área de estacionamientos con sombreado natural.", type: 5, evidence: true},
+        
     ]
     
     return (
