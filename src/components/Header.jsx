c2e631b5851184c7faf9250c1783d8af504d1fea
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-06-30 09:40:35 -0600

    Finishing basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 "use client";
 import Navbar from "./Navbar";
 import { Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 function Header() {
     return (
         <div className="header">
             <div className="header-container">
+                <div className="header-logo">
+                    <Image src="/GreenMetric/agenda-white.svg" alt="logo" />
+                    <Image src="/GreenMetric/green-metric.svg" alt="logo" />
+                </div>
                 <div className="titles no-overflow">
                     <Text className="font-header">Universidad Autónoma de San Luis Potosí</Text>
                     <Text className="font-header">Agenda Ambiental</Text>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { Box } from "@chakra-ui/react"
+import { Box, Image } from "@chakra-ui/react"
 
 export default function Navbar() {
     return (
@@ -12,34 +12,40 @@ export default function Navbar() {
             </Link>
             <Link href="/infraestructura/">
                 <Box w="100%" bgColor={"#008037"}>
-                    <h3>Infraestructura y escenarios</h3>
+                    <Image src="/GreenMetric/navbar/edificio.png"/>
+                    <h3>Entorno e Infraestructura</h3>
                 </Box>
             </Link>
             <Link href="/energia/">
                 <Box w="100%" bgColor={"#cfaf2e"}>
-                    <h3>Energía y cambio climático</h3>
+                    <Image src="/GreenMetric/navbar/climatico.png"/>
+                    <h3>Energía y Cambio Climático</h3>
                 </Box>
 
             </Link>
             <Link href="/residuos/">
 
                 <Box w="100%" bgColor={"#e09d9a"}>
-                    <h3>Residuos</h3>
+                    <Image src="/GreenMetric/navbar/basura.png"/>
+                    <h3>Gestión de Residuos</h3>
                 </Box>
             </Link>
             <Link href="/agua/">
                 <Box w="100%" bgColor={"#5271ff"}>
-                    <h3>Agua</h3>
+                    <Image src="/GreenMetric/navbar/agua.png"/>
+                    <h3>Uso de Agua</h3>
                 </Box>
             </Link>
             <Link href="/transporte/">
                 <Box w="100%" bgColor={"#ff1616"}>
+                    <Image src="/GreenMetric/navbar/transporte.png"/>
                     <h3>Transporte</h3>
                 </Box>
             </Link>
             <Link href="/educacion/">
                 <Box w="100%" bgColor={"#ff914d"}>
-                    <h3>Educación</h3>
+                    <Image src="/GreenMetric/navbar/investigacion.png"/>
+                    <h3>Educación e Investigación</h3>
                 </Box>
             </Link>
         </div>
