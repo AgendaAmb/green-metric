90f053e916b35659102c096c385e595e2d85c9db
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-07-11 14:50:24 -0600

    tet

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const tSlash = (process.env.PROD === "true" ? true : false);
 
 const config = {
-    basePath: '/GreemMetricTest',
+    basePath: '/GreenMetricTest',
     trailingSlash: tSlash,
 }
 
