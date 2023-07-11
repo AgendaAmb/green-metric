298b8fcff676d7f52a18579f6b3e8ac7607f57db
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-07-11 13:35:15 -0600

    Production

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@
 const tSlash = (process.env.PROD === "true" ? true : false);
 
 const config = {
-    //trailingSlash: tSlash,
-    basePath: "/GreenMetricTest",
-    async rewrites() {
-        return [
-            {
-                source: '/',
-                destination: '/GreenMetricTest/informacion',
-            },
-        ]
-    }
+    trailingSlash: tSlash,
 }
 
 const nextConfig = config;
