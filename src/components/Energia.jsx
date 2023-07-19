c66927bf7b7e6cd0d838c5aba21389ca6d6123ea
Oscarucho2 <oskarmartinez15@gmail.com>
2023-07-19 12:31:11 -0600

    Agregando ID  a las respuestas

--- src/components/Energia.jsx
+++ src/components/Energia.jsx
@@ -23,14 +23,14 @@ export default function Energia(){
     const [components2,setComponents2] = useState([]);
 
     const item = () => {
-        const newMod = [...components,{text: 'Tipo de energía renovable', type:1, question:'2-2-1-7'}, 
-        {text:'Cantidad de equipos', type:2, question:'2-2-1-8'}, {text:'Capacidad de kW/h', type:2, question:'2-2-1-9'}, {evidence:true, maxPhotos:1}];
+        const newMod = [...components,{text: 'Tipo de energía renovable', type:1, question:'a-2-2-1-7'}, 
+        {text:'Cantidad de equipos', type:2, question:'a-2-2-1-8'}, {text:'Capacidad de kW/h', type:2, question:'a-2-2-1-9'}, {evidence:true, maxPhotos:1}];
         setComponents([...newMod]);
     }
     
      const item2 = () => {
-        const newMod2 = [...components2,{text: 'Nombre de política', type:1, question:'2-2-2-3'}, 
-        {text:'Institución que lo valida', type:1, question:'2-2-2-4'}, {evidence:true, maxPhotos:3}];
+        const newMod2 = [...components2,{text: 'Nombre de política', type:1, question:'a-2-2-2-3'}, 
+        {text:'Institución que lo valida', type:1, question:'a-2-2-2-4'}, {evidence:true, maxPhotos:3}];
         setComponents2([...newMod2]);
     }
 
@@ -67,22 +67,22 @@ export default function Energia(){
                 <h3 className="expand-2 green">2.1 Indicar en cada espacio, la cantidad de forma numérica de mobiliario de eficiencia energética y el número de aplicaciones convencionales que usa</h3>
                 
                 <Stack spacing="30px">
-                    <ModuleInput title="Iluminación" elements={[{text: '2.1.1 Descarga de alta intensidad', type:2, question:'2-1-1'}, 
-                    {text:'2.1.2 LED', type:2, question:'2-1-2'}, {text:'2.1.3 Fluorescente', type:2, question:'2-1-3'}, {text:'2.1.4 Inducción', type:2, question:'2-1-4'}, {evidence:true, maxPhotos:3}]} />
+                    <ModuleInput title="Iluminación" elements={[{text: '2.1.1 Descarga de alta intensidad', type:2, question:'a-2-1-1'}, 
+                    {text:'2.1.2 LED', type:2, question:'a-2-1-2'}, {text:'2.1.3 Fluorescente', type:2, question:'a-2-1-3'}, {text:'2.1.4 Inducción', type:2, question:'a-2-1-4'}, {evidence:true, maxPhotos:3}]} />
                 </Stack>
                 <Stack spacing="30px">
-                    <ModuleInput title="Refrigeradores" elements={[{text: '2.1.5 Convencional', type:2 , question:'2-1-5'}, 
-                    {text:'2.1.6 De alta eficiencia', type:2, question:'2-1-6'}, {text:'2.1.7 De laboratorio', type:2, question:'2-1-7'},{evidence:true, maxPhotos:3}]} />
+                    <ModuleInput title="Refrigeradores" elements={[{text: '2.1.5 Convencional', type:2 , question:'a-2-1-5'}, 
+                    {text:'2.1.6 De alta eficiencia', type:2, question:'a-2-1-6'}, {text:'2.1.7 De laboratorio', type:2, question:'a-2-1-7'},{evidence:true, maxPhotos:3}]} />
                 </Stack>
                 <Stack spacing="30px">
                     <ModuleInput title="Generadores eléctricos" elements={[
-                    {text: '2.1.8 Generador inverter', type:2, question:'2-1-8'}, 
-                    {text:'2.1.9 Generador por condensador', type:2, question:'2-1-9'}, {text:'2.1.10 Generador por regulador electrónico', type:2, question:'2-1-10'},{evidence:true, maxPhotos:3}]} />
+                    {text: '2.1.8 Generador inverter', type:2, question:'a-2-1-8'}, 
+                    {text:'2.1.9 Generador por condensador', type:2, question:'a-2-1-9'}, {text:'2.1.10 Generador por regulador electrónico', type:2, question:'a-2-1-10'},{evidence:true, maxPhotos:3}]} />
                 </Stack>
                 <Stack spacing="30px">
                     <ModuleInput title="Aire acondicionado" elements={[
-                    {text: '2.1.11 Convencional', type:2, question:'2-1-11'}, 
-                    {text:'2.1.12 Con tecnología inverter', type:2, question:'2-1-12'}, {text:'2.1.13 Aire lavado', type:2, question:'2-1-13'},{evidence:true, maxPhotos:3}]} />
+                    {text: '2.1.11 Convencional', type:2, question:'a-2-1-11'}, 
+                    {text:'2.1.12 Con tecnología inverter', type:2, question:'a-2-1-12'}, {text:'2.1.13 Aire lavado', type:2, question:'a-2-1-13'},{evidence:true, maxPhotos:3}]} />
                 </Stack>
             </Stack>
             <Stack spacing="30px" w="100%" className="column-2">
@@ -106,11 +106,11 @@ export default function Energia(){
                     "Control de iluminación automático (sensores de movimiento).",
                     "Ajuste de protección solar (ventanas polarizadas,pergolas).",
                     "Sistemas pasivos para el aprovechamiento de la luz natural (Ventanas y tragaluces).",
-                    ]}} questionCheckBox={'2-1-14'}/>
+                    ]}} questionCheckBox={'a-2-1-14'}/>
             </Stack>
             <Stack spacing="30px" w="100%"   padding="30px">
             <ModuleInput title="2.1.15 Área total ocupada por Smart Buildings dentro de su entidad o espacio de trabajo"
-                elements={[{type:5, question:'2-1-15'}]} /> 
+                elements={[{type:5, question:'a-2-1-15'}]} /> 
             </Stack>
             <Stack spacing="30px" w="100%" className="column" position={"relative"}>
             <Stack direction={"row"} gap={6} position={"absolute"} right={"20px"}>
@@ -122,16 +122,16 @@ export default function Energia(){
             <h5 className="expand-2 blue">Coloque la información solicitada en los recuadros y añada evidencia fotográfica del recuadro de mayor tamaño</h5>
             <Stack w="100%" direction={"row"} divider={<StackDivider borderColor="gray.200" />}>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'2-2-1-1'}, 
-                    {text:'Cantidad de equipos', type:2, question:'2-2-1-2'}, {text:'Capacidad de kW/h', type:2, question:'2-2-1-3'}, {evidence:true, maxPhotos:1}]} />
+                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'a-2-2-1-1'}, 
+                    {text:'Cantidad de equipos', type:2, question:'a-2-2-1-2'}, {text:'Capacidad de kW/h', type:2, question:'a-2-2-1-3'}, {evidence:true, maxPhotos:1}]} />
                 </Stack>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'2-2-1-4'}, 
-                    {text:'Cantidad de equipos', type:2, question:'2-2-1-5'}, {text:'Capacidad de kW/h', type:2, question:'2-2-1-6'}, {evidence:true, maxPhotos:1}]} />
+                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'a-2-2-1-4'}, 
+                    {text:'Cantidad de equipos', type:2, question:'a-2-2-1-5'}, {text:'Capacidad de kW/h', type:2, question:'a-2-2-1-6'}, {evidence:true, maxPhotos:1}]} />
                 </Stack>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'2-2-1-7'}, 
-                    {text:'Cantidad de equipos', type:2, question:'2-2-1-8'}, {text:'Capacidad de kW/h', type:2, question:'2-2-1-9'}, {evidence:true, maxPhotos:1}]} />
+                    <ModuleInput elements={[{text: 'Tipo de energía renovable', type:1, question:'a-2-2-1-7'}, 
+                    {text:'Cantidad de equipos', type:2, question:'a-2-2-1-8'}, {text:'Capacidad de kW/h', type:2, question:'a-2-2-1-9'}, {evidence:true, maxPhotos:1}]} />
                 </Stack>
 
                 <Stack spacing="30px">
@@ -147,12 +147,12 @@ export default function Energia(){
             </Stack>
             <h4 className="expand-2 blue">2.2.2 Certificación nacional o internacional sobre políticas en la implementación de edificios verdes. Adjuntar evidencia que lo valide</h4>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{text: 'Nombre de política', type:1, question:'2-2-2-1'}, 
-                    {text:'Institución que lo valida', type:1, question:'2-2-2-2'}, {evidence:true, maxPhotos:3}]} />
+                    <ModuleInput elements={[{text: 'Nombre de política', type:1, question:'a-2-2-2-1'}, 
+                    {text:'Institución que lo valida', type:1, question:'a-2-2-2-2'}, {evidence:true, maxPhotos:3}]} />
                 </Stack>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{text: 'Nombre de política', type:1, question:'2-2-2-3'}, 
-                    {text:'Institución que lo valida', type:1, question:'2-2-2-4'}, {evidence:true, maxPhotos:3}]} />
+                    <ModuleInput elements={[{text: 'Nombre de política', type:1, question:'a-2-2-2-3'}, 
+                    {text:'Institución que lo valida', type:1, question:'a-2-2-2-4'}, {evidence:true, maxPhotos:3}]} />
                 </Stack>
                 <Stack spacing="30px">
                     <ModuleInput elements={components2}/>
@@ -170,71 +170,71 @@ export default function Energia(){
                 "Programa(s) tiene como objetivo reducir uno de los tres alcances de emisiones (Alcance 1 o 2 o 3).",
                 "Los programas tienen como objetivo reducir dos de los tres alcances de emisiones (Alcance 1 y 2 o Alcance 1 y 3 o Alcance 2 y 3).",
                 "Los programas tienen como objetivo reducir las emisiones de los tres alcances (Alcance 1, 2 y 3)",
-                ]} questionSelect="2-2-3"/>
+                ]} questionSelect="a-2-2-3"/>
             </Stack>
 
             <Stack spacing="30px" w="100%" padding="30px">
             <h4 className="expand-2 blue">2.2.4 Número de programas de innovación en energía y cambio climático (Implementación de programas de seguridad energética, programas de ahorro de energía, programa de eficiencia energética, sosteibilidd energética, etc).</h4>
                 <Stack spacing="30px">
-                    <ModuleInput elements={[{type:2, question:'2-2-4'}, {evidence:true, maxPhotos:3}]} />
+                    <ModuleInput elements={[{type:2, question:'a-2-2-4'}, {evidence:true, maxPhotos:3}]} />
                  </Stack>
             </Stack>
 
             <Stack spacing="30px" w="100%" padding="30px" className="column-2">
             <h4 className="expand-2 blue">2.2.5 ¿Su entidad cuenta con programas que enfrentan los riesgos del cambio climático? ¿Cuál es su condición de implementación?</h4>
             <Stack spacing="30px">
-                <ModuleInput title="2.2.5.1 3R (Reducir, Reusar, Reciclar)" elements={[{dicotomic:true, span:true, questionDicotomic:'2-2-5-1'}]} />
+                <ModuleInput title="2.2.5.1 3R (Reducir, Reusar, Reciclar)" elements={[{dicotomic:true, span:true, questionDicotomic:'a-2-2-5-1'}]} />
                 <ModuleInput select={[
                 "Sin programa implementado",
                 "Programa en preparación",
                 "Capacitación y material educativo a nivel local",
                 "Capacitación y material educativo a nivel local y nacional",
                 "Capacitación y material educativo a nivel local, nacional y internacional",
-                ]} questionSelect="2-2-5-1-1"/>
+                ]} questionSelect="a-2-2-5-1-1"/>
                 <ModuleInput elements={[{evidence:true, maxPhotos:3}]}/>            
                 </Stack>
                 <Stack spacing="30px">
-                <ModuleInput title="2.2.5.2 Programa de seguridad energética" elements={[{dicotomic:true, span:true, questionDicotomic:'2-2-5-2'}]} />
+                <ModuleInput title="2.2.5.2 Programa de seguridad energética" elements={[{dicotomic:true, span:true, questionDicotomic:'a-2-2-5-2'}]} />
                 <ModuleInput select={[
                 "Sin programa implementado",
                 "Programa en preparación",
                 "Capacitación y material educativo a nivel local",
                 "Capacitación y material educativo a nivel local y nacional",
                 "Capacitación y material educativo a nivel local, nacional y internacional",
-                ]} questionSelect="2-2-5-2-2"/>
+                ]} questionSelect="a-2-2-5-2-2"/>
                 <ModuleInput elements={[{evidence:true, maxPhotos:3}]}/>            
                 </Stack>
                 <Stack spacing="30px">
-                <ModuleInput title="2.2.5.3 Mitigación de cambio climático" elements={[{dicotomic:true, span:true, questionDicotomic:'2-2-5-3'}]} />
+                <ModuleInput title="2.2.5.3 Mitigación de cambio climático" elements={[{dicotomic:true, span:true, questionDicotomic:'a-2-2-5-3'}]} />
                 <ModuleInput select={[
                 "Sin programa implementado",
                 "Programa en preparación",
                 "Capacitación y material educativo a nivel local",
                 "Capacitación y material educativo a nivel local y nacional",
                 "Capacitación y material educativo a nivel local, nacional y internacional",
-                ]} questionSelect="2-2-5-3-2"/>
+                ]} questionSelect="a-2-2-5-3-2"/>
                 <ModuleInput elements={[{evidence:true, maxPhotos:3}]}/>            
                 </Stack>
                 <Stack spacing="30px">
-                <ModuleInput title="2.2.5.4 Reducción del uso de papel y plástico" elements={[{dicotomic:true, span:true, questionDicotomic:'2-2-5-4'}]} />
+                <ModuleInput title="2.2.5.4 Reducción del uso de papel y plástico" elements={[{dicotomic:true, span:true, questionDicotomic:'a-2-2-5-4'}]} />
                 <ModuleInput select={[
                 "Sin programa implementado",
                 "Programa en preparación",
                 "Capacitación y material educativo a nivel local",
                 "Capacitación y material educativo a nivel local y nacional",
                 "Capacitación y material educativo a nivel local, nacional y internacional",
-                ]} questionSelect="2-2-5-4-2"/>
+                ]} questionSelect="a-2-2-5-4-2"/>
                 <ModuleInput elements={[{evidence:true, maxPhotos:3}]}/>            
                 </Stack>
                 <Stack spacing="30px">
-                <ModuleInput title="2.2.5.5 Advertencia temprana del cambio climático" elements={[{dicotomic:true, span:true, questionDicotomic:'2-2-5-5'}]} />
+                <ModuleInput title="2.2.5.5 Advertencia temprana del cambio climático" elements={[{dicotomic:true, span:true, questionDicotomic:'a-2-2-5-5'}]} />
                 <ModuleInput select={[
                 "Sin programa implementado",
                 "Programa en preparación",
                 "Capacitación y material educativo a nivel local",
                 "Capacitación y material educativo a nivel local y nacional",
                 "Capacitación y material educativo a nivel local, nacional y internacional",
-                ]} questionSelect="2-2-5-5-2"/>
+                ]} questionSelect="a-2-2-5-5-2"/>
                 <ModuleInput elements={[{evidence:true, maxPhotos:3}]}/>
                 </Stack>
             </Stack>
