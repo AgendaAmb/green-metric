a960996e9477917824c25b7609d68d6b91b2637a
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-07-20 12:57:38 -0600

    Fixing fetching data

--- src/app/providers.js
+++ src/app/providers.js
@@ -13,53 +13,39 @@ import axios from "axios";
 export const FormContext = createContext(null);
 
 function FormBase({ children, handleSubmit, handleChange, values }) {
-    console.log("valores", values);
+    console.log("valores", values.get);
     const [submitFunction, setSubmitFunction] = useState(handleSubmit);
-    const [data, setData] = useState({ values, handleChange, setSubmitFunction, submitFunction });
-    const fetchData = () => {
-        try {
-            const response = axios.get('/GreenMetric/api/answers', {
-                params: { value: 1 } // Pasar los parámetros como parte del objeto `params`
-            }).then((res) => {
-                //console.log(res.data);
-                let result = [];
-                let i = 0;
-                for (let el of res.data) {
-                    let obj = {};
-                    obj[el.question_id] = el.answer
-                    result[i++] = obj;
-                }
-                setData(...data, result);
-            })
-                .catch((e) => {
-                    console.log("Error con la BD");
-                })
-        } catch (error) {
-            console.log("Error con la request");
-        }
-    };
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+    useEffect(async() => {
         //fetchData();
-
+        const vals = await values;
+        console.log("vals", vals);
+        setData({ values :vals, handleChange, setSubmitFunction, submitFunction });
+        setLoading(false);
     }, []);
 
     const contextValue = useMemo(() => data, [data]);
-    return (
+    if(loading) return (<div>Loading...</div>)
+    else{
+        return (
+    
+            <FormContext.Provider value={contextValue}>
+                <Stack onSubmit={handleSubmit} as={"form"} className="width-100">
+                    <DndProvider backend={HTML5Backend}>
+                        <CacheProvider>
+                            <ChakraProvider>
+                                {children}
+                            </ChakraProvider>
+                        </CacheProvider>
+                    </DndProvider>
+                </Stack>
+            </FormContext.Provider>
+    
+        );
 
-        <FormContext.Provider value={contextValue}>
-            <Stack onSubmit={handleSubmit} as={"form"} className="width-100">
-                <DndProvider backend={HTML5Backend}>
-                    <CacheProvider>
-                        <ChakraProvider>
-                            {children}
-                        </ChakraProvider>
-                    </CacheProvider>
-                </DndProvider>
-            </Stack>
-        </FormContext.Provider>
-
-    );
+    }
 }
 
 export const Providers = withFormik({
@@ -67,8 +53,8 @@ export const Providers = withFormik({
         const response = axios.get('/GreenMetric/api/answers', {
             params: { value: 1 } // Pasar los parámetros como parte del objeto `params`
         });
-        const data = await response.data;
-        return data;
+        const results = await response;
+        return results.data;
     },
 
     // Custom sync validation
@@ -84,34 +70,22 @@ export const Providers = withFormik({
         return errors;
     },
 
-    /* fetchData: async () => {
-        try {
-            const response = await axios.get('/GreenMetric/api/answers', { value: 1 }) // Reemplaza '/api/endpoint' por tu ruta de API correcta
-            const data = response.data;
-            console.log(data); // Haz algo con los datos obtenidos de la API
-        } catch (error) {
-            console.error(error);
-        }
-    },
- */
     handleSubmit: (values, { setSubmitting }) => {
         setTimeout(() => {
             //Checking if form is login
             const isLogin = values?.hasOwnProperty("cve_rpe") && values?.hasOwnProperty("pwd_login");
-            //console.log("Values: ", values);
-            //console.log(isLogin);
+            
             if (isLogin) {
                 const id = values.cve_rpe || "";
                 const pwd = values.pwd_login || "";
-                /* console.log(id);
-                console.log(pwd); */
+                
                 axios.post("/GreenMetric/api/auth", { params: { user_id: id, password: pwd } }).then((res) => { }).catch((e) => {
                     console.log("error")
                 });
             }
             else {
                 //alert(JSON.stringify(values, null, 2));
-                console.log(values);
+                //console.log(values);
                 for (const [key, value] of Object.entries(values)) {
                     axios.post('/GreenMetric/api/answers', {
                         value: value,
@@ -125,7 +99,6 @@ export const Providers = withFormik({
                         });
                 }
             }
-            //window.alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
         }, 100);
     },
