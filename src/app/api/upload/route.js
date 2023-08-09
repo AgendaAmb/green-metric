e8d70105839b5e3efc6483c62a7ad25e33ffc5c4
Edwin Saucedo <edwin-saucedo@hotmail.com>
2023-08-09 13:52:56 -0600

    Almost finished on saving imgs

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -2,19 +2,27 @@ import fs from "fs";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const formData = await req.formData();
-
-  const formDataEntryValues = Array.from(formData.values());
-  for (const formDataEntryValue of formDataEntryValues) {
-    if (typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
-      const file = formDataEntryValue;
-      const ext = file.name.split(".").pop();
-      console.log(ext);
-      fs.mkdirSync("public/answers/1", { recursive: true });
-      const buffer = Buffer.from(await file.arrayBuffer());
-      fs.writeFileSync(`public/answers/1/${"test"}.${ext}`, buffer);
+  
+  try {
+    const formData = await req.formData();
+    const user = JSON.parse(req.cookies.get("user").value);
+    const formDataEntryValues = Array.from(formData.values());
+    for (const formDataEntryValue of formDataEntryValues) {
+      if (typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
+        const file = formDataEntryValue;
+        const ext = file.name.split(".").pop();
+        console.log(ext);
+        fs.mkdirSync(`public/answers/${user.dependency_id}`, { recursive: true });
+        const buffer = Buffer.from(await file.arrayBuffer());
+        fs.writeFileSync(`public/answers/${user.dependency_id}/${"test"}.${ext}`, buffer);
+      }
     }
+    return NextResponse.json({ success: true });
   }
-  return NextResponse.json({ success: true });
+  catch (e) {
+    return NextResponse.json({ success: false });
+  }
+
+
 };
 
